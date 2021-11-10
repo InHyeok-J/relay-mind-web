9d4dc24bdb06237b39e2d78239858ff36993cfd3
inhyeok <ss112d86@gmail.com>
2021-11-11 05:03:09 +0900

    Feature: 세션정보 조회

--- back/src/routes/user.ts
+++ back/src/routes/user.ts
@@ -1,9 +1,10 @@
 import { isNotLoggedIn, isLoggedIn } from './../middleware/authHandler';
 import * as express from 'express';
-import { signUp, login, logout } from '../controllers/user';
+import { signUp, login, logout, getUser } from '../controllers/user';
 
 const router = express.Router();
 
+router.get('/', getUser);
 router.post('/signup', isNotLoggedIn, signUp);
 router.post('/login', isNotLoggedIn, login);
 router.get('/logout', isLoggedIn, logout);
