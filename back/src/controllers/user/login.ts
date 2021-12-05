878c461fec86e9075c6e9978e641750df2d72ab5
inhyeok <ss112d86@gmail.com>
2021-12-06 00:38:29 +0900

    Refactor: User라우터 customError 적용

--- back/src/controllers/user/login.ts
+++ back/src/controllers/user/login.ts
@@ -1,17 +1,36 @@
+import { CustomError } from './../../utils/customError';
 import catchAsync from '../../utils/catchAsync';
 import * as passport from 'passport';
 
 export const login = catchAsync(async (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
         if (err) {
-            return next(err);
+            const customError = new CustomError(
+                500,
+                'session',
+                'passportError',
+                err
+            );
+            return next(customError);
         }
         if (!user) {
-            return res.customFail(400, info.message);
+            const customError = new CustomError(
+                401,
+                'exits',
+                info.message,
+                err
+            );
+            return next(customError);
         }
         req.login(user, (err) => {
             if (err) {
-                next(err);
+                const customError = new CustomError(
+                    500,
+                    'session',
+                    'passportError',
+                    err
+                );
+                return next(customError);
             }
             return res.customSuccess(200, '로그인성공', {
                 ...user,
