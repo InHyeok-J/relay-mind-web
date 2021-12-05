e6899d27f0f4ab810cdd171471b674cd0a9c4e59
inhyeok <ss112d86@gmail.com>
2021-12-06 02:33:51 +0900

    Fix: Redis 버전문제로 다운그레이드4->3.12

--- back/src/database/redisClient.ts
+++ back/src/database/redisClient.ts
@@ -3,12 +3,10 @@ import env from '../configs';
 
 const client = createClient({
     url: env.REDIS_URL,
-    legacyMode: true,
 });
 
 (async () => {
     client.on('error', (err) => console.log('Redis Client Error', err));
-    await client.connect();
 })();
 
 export default client;
