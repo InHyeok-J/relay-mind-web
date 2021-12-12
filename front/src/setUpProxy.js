95c01094ce68e2c36054322af78df78bca270c75
YongRae_Kim (Usage) <kyr9389@naver.com>
2021-12-13 01:44:47 +0900

    feat: 웹 소켓 프록시 적용

--- front/src/setUpProxy.js
+++ front/src/setUpProxy.js
@@ -2,10 +2,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:4000',
+        createProxyMiddleware(['/api', '/socket.io'], {
+            target: '*',
             changeOrigin: true,
+            ws: true,
+            router: {
+                '/socket.io': 'ws://*',
+            },
         }),
     );
 };
